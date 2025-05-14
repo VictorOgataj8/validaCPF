5562cf8df95087ac22387d70dae09939c50b818c
Victor <victor.ogataj8@gmail.com>
2025-05-14 19:28:04 -0300

    js atualizado e css mais bonito

--- script.js
+++ script.js
@@ -1,48 +1,60 @@
-function validarCPF(cpf) {
+function validarCPF(cpf){
     cpf = cpf.replace(/[^\d]+/g,"");
-    if(cpf.lenght !== 11 || /^(\d)\1+$/.test(cpf)){
+
+    if(cpf.length !== 11 || /[^\d]+\1+$/.test(cpf)){
         return false;
     }
+
     let soma = 0;
     let resto;
-    
-    for(let i=1; i < 9; i++){
-        soma+= parseInt(cpf.substring(i-1,i)) * (11 - i);
-    }
-    resto = (soma * 10) % 11;
-    if(resto ===10 || resto ===11)
-    {
-        resto=0;                       
-    }
-    if(resto !== parseInt(cpf.substring(9,10))){
-        return false;
-    }
-    soma = 0;
-    for(let i=1; i < 10; i++){
-        soma+= parseInt(cpf.substring(i-1,i)) * (12 - i);
-    }
-    resto = (soma *10 ) % 11;
 
-    if(resto === 10||resto ===11)
+    // Validação do 1 DV
+    for(let i=1; i <= 9; i++){
+        soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11
+
+    if(resto === 10 || resto === 11)
     {
         resto = 0
+    }
+    if(resto !== parseInt(cpf.substring(9, 10))){
+        return false;
+    }
+
+    // Validação do 2 DV
+    soma = 0;
+    for(let i = 1; i <=10; i++){
+        soma += parseInt(cpf.substring(i -1, i)) * (12 - i);
+
+    }
+    resto = (soma * 10) % 11;
+
+    if(resto === 10 || resto === 11)
+    {
+        resto = 0;
     }
     if(resto !== parseInt(cpf.substring(10,11))){
         return false;
     }
-    return true;
-}   
 
-document.getElementById("cpf").addEventListener("submit", function(e){
+    return true; 
+ }
+
+ document.getElementById("cpfForm");
+ addEventListener("submit", function(e){
     e.preventDefault();
     const cpfInput = document.getElementById("cpf").value;
     const messageDiv = document.getElementById("message");
+
     if(validarCPF(cpfInput)){
-        messageDiv.textContent = "cpf valido";
-        messageDiv.className = "message sucess";
+        messageDiv.textContent = "CPF Válida";
+        messageDiv.className = "message success";
     } else{
-        messageDiv.textContent = "cpf invalido";
-        messageDiv.className = "message error";
+        messageDiv.textContent = "CPF Inválido";
+        messageDiv.className ="message error";
     }
-    messageDiv.style.display = "block"
-})
+
+    messageDiv.style.display = "block";
+ });
